170d317b7569df49bf815cb46ff99b1b22df048d
arthu <arthur.oganezov@gmail.com>
2016-11-19 16:14:08 -0600

    filtering is workinggit add filter.js! Sort ofgit add filter.js!

--- filter.js
+++ filter.js
@@ -1,19 +1,24 @@
 window.onload = function () {
+    var low = ["TRUMP", "DONALD TRUMP", "HILLARY CLINTON", "CLINTON", "KILLARY", "NOT MY PRESIDENT"];
+    var med = [""];
+    var high = ["SENATOR", "WASHINGTON D.C.", "POLITICS"];
+    var currentSetting = low;
     /*
      * Created by arthu on 11/19/2016.
      */
     console.log("ready!");
     if (document.getElementsByClassName("_5jmm")) {
         var elem = document.getElementsByClassName("_5jmm");
-        for (var i = 0; i < elem.length; i++) {
-            var postText = elem[i].textContent;
-            
-            if (postText.indexOf("Will be working") !== -1) {
-                console.log("FOUND: " + postText);
-                elem[i].parentElement.removeChild(elem[i]);
-            }
-            else {
-                console.log("ACTUALLY FOUND: " + postText);
+        for (var i = 0; i < elem.length; i+2) {
+            var postText = elem[i].textContent.toUpperCase();
+            for (var j = 0; j < currentSetting.length; j++) {
+                if (postText.indexOf(currentSetting[j]) !== -1) {
+                    console.log("FOUND: " + postText);
+                    elem[i].parentElement.removeChild(elem[i]);
+                }
+                else {
+                    console.log("ACTUALLY FOUND: " + postText);
+                }
             }
         }
     }
