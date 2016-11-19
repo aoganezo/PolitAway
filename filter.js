ad138d4a2bb655a60fff84992d98147eee9fb79d
arthu <arthur.oganezov@gmail.com>
2016-11-19 12:26:56 -0600

    filtering is working, now to specify what we're filtering...

--- filter.js
+++ filter.js
@@ -1,19 +1,31 @@
-/**
- * Created by arthu on 11/19/2016.
- */
-
-console.log( "ready!" );
-if (document.getElementsByClassName("_5jmm")) {
-    var elem = document.getElementsByClassName("_5jmm");
-    console.log(elem[0].textContent);
-        /*for (var i = 0; i < elem.length; i++) {
-            if (elem[i].querySelector('div').textContent.contains("Bill")) {
-                alert("FOUND THIS");
-                console.log(elem[i]);
+window.onload = function () {
+    /*
+     * Created by arthu on 11/19/2016.
+     */
+    console.log("ready!");
+    if (document.getElementsByClassName("_5jmm")) {
+        var elem = document.getElementsByClassName("_5jmm");
+        for (var i = 0; i < elem.length; i++) {
+            var postText = elem[i].textContent;
+            
+            if (postText.indexOf("Will be working") !== -1) {
+                console.log("FOUND: " + postText);
+                elem[i].parentElement.removeChild(elem[i]);
             }
-        }*/
-
- //   if (document.getElementById("hyperfeed_story_id_").textContent.contains("Bill")) {
-   //     alert("FOUND THIS");
-    //}
+            else {
+                console.log("ACTUALLY FOUND: " + postText);
+            }
+        }
+    }
+    /*else if (document.getElementsByClassName("userContentWrapper")) {
+     console.log("ready!2");
+     var ele = document.getElementsByClassName("userContentWrapper");
+     for (var j = 0; j < ele.length; j++) {
+     var postText2 = elem[i].textContent;
+     if (postText2.indexOf("a") !== -1) {
+     console.log("FOUND: " + postText2);
+     }
+     else {
+     console.log("ACTUALLY FOUND: " + postText2);
+     }}}*/
 }
