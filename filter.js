3fa781b9924e2628c588df06b19eea9cc77794e7
arthu <arthur.oganezov@gmail.com>
2016-11-19 06:21:16 -0600

    Able to access post text now

--- filter.js
+++ filter.js
@@ -2,15 +2,18 @@
  * Created by arthu on 11/19/2016.
  */
 
-    console.log( "ready!" );
-
-
-console.log("Facebook Loaded");
-alert("HELLO");
+console.log( "ready!" );
 if (document.getElementsByClassName("_5jmm")) {
     var elem = document.getElementsByClassName("_5jmm");
-    console.log(elem);
-    if (document.getElementById("hyperfeed_story_id_*").textContent.contains("Bill")) {
-        alert("FOUND THIS");
-    }
+    console.log(elem[0].textContent);
+        /*for (var i = 0; i < elem.length; i++) {
+            if (elem[i].querySelector('div').textContent.contains("Bill")) {
+                alert("FOUND THIS");
+                console.log(elem[i]);
+            }
+        }*/
+
+ //   if (document.getElementById("hyperfeed_story_id_").textContent.contains("Bill")) {
+   //     alert("FOUND THIS");
+    //}
 }
