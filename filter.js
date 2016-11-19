ccd2e9805a9a2f3b3935625c5e16a4ea3813047e
arthu <arthur.oganezov@gmail.com>
2016-11-19 03:47:32 -0600

    Making progress on access certain parts.

--- filter.js
+++ filter.js
@@ -4,5 +4,8 @@
 console.log("Facebook Loaded");
 alert("HELLLO");
 console.log(document.querySelector('body'));
-var element = document.querySelector('body');
-element.parentElement.removeChild(element);
+//var element = document.querySelector('body');
+var ele = document.getElementsByClassName("_1dwg");
+//element.parentElement.removeChild(element);
+console.log(ele);
+ele[0].parentNode.removeChild(ele[0]);
