53098d5f9f2cc39ad06b24156cba384f6bc57408
arthu <arthur.oganezov@gmail.com>
2016-11-20 03:10:13 -0600

    filter.js is commented/documented.

--- filter.js
+++ filter.js
@@ -1,29 +1,37 @@
 /*
- * Created by arthu on 11/19/2016.
+ * Created by artur on 11/19/2016.
  */
-
 function removeItems() {
-    var low = ["TRUMP", "CLINTON", "KILLARY", "NOT MY PRESIDENT"];
-    var med = ["PENCE"];
-    var high = ["SENATOR", "WASHINGTON D.C.", "POLITICS", "PENCE"];
+    //Low is the least aggressive setting, that is it removes the WORST posts.
+    var low = [/*low*/"TRUMP", "CLINTON", "KILLARY", "NOT MY PRESIDENT"];
+
+    //Med is medium aggressiveness, it includes all low settings, and a few more.
+    var med = [/*med:*/"PENCE", "OBAMA", "DEMOCRAT", "REPUBLICAN", "LIBERTARIAN",
+        /*low:*/"TRUMP", "CLINTON", "KILLARY", "NOT MY PRESIDENT"];
+
+    //High is the most rigorous. You really want nothing to do with politics.
+    var high = [/*high:*/"SENATOR", "WASHINGTON D.C.", "POLITICS",
+        /*med:*/"PENCE", "OBAMA", "DEMOCRAT", "REPUBLICAN", "LIBERTARIAN",
+        /*low*/"TRUMP", "CLINTON", "KILLARY", "NOT MY PRESIDENT"];
+
     // function ext("options.js", () {
     //     (currentSetting = "level()");
     //     var currentSetting = low;
     // }
 // );
-    var found = 0;
-
-    localStorage.getItem("agressivness");
-    console.log(agressivness);
+    var found = 0; //Counts how many posts it has removed. Not 100% accurate.
+    var currentSetting = low; //Selects the current setting to use.
+    localStorage.getItem("aggressiveness");
+    console.log(currentSetting);
     
-    if (document.getElementsByClassName("_5jmm")) {
-        var elem = document.getElementsByClassName("_5jmm"); // + has one of the words desire to filter out
-        for (var i = 0; i < elem.length; i++) {
-            var postText = elem[i].textContent.toUpperCase();
-            for (var j = 0; j < currentSetting.length; j++) {
-                if (postText.indexOf(currentSetting[j]) !== -1) {
-                    if (postText.indexOf(currentSetting[j]) < postText.indexOf("COMMENTS")) {
-                        elem[i].parentElement.removeChild(elem[i]);
+    if (document.getElementsByClassName("_5jmm")) { //Checks if it's a post on a feed. "_5jmm" seems to be on all posts.
+        var feed = document.getElementsByClassName("_5jmm"); // Assigns the feed array to a var.
+        for (var i = 0; i < feed.length; i++) { //Loops through all posts.
+            var postText = feed[i].textContent.toUpperCase(); //Assigns this posts text to a var.
+            for (var j = 0; j < currentSetting.length; j++) { //Loops through keywords.
+                if (postText.indexOf(currentSetting[j]) !== -1) { //Checks if a word from keywords is in the post.
+                    if (postText.indexOf(currentSetting[j]) < postText.indexOf("COMMENTS")) { //Makes sure word isn't in comments.
+                        feed[i].parentElement.removeChild(feed[i]); //Removes the post.
                         console.log("found: " + found++);
                     }
                 }
@@ -31,18 +39,11 @@ function removeItems() {
         }
     }
 }
-
-window.onscroll = function() {
-
+window.onscroll = function() { //Function keeps going whenever you scroll to continuously delete posts w/o refresh.
     removeItems();
 };
-
-window.onload = function () {
-    setTimeout(function () {
+window.onload = function () { //Function runs once onLoad in case any posts need to be removed immediately.
+    setTimeout(function () { //Makes it wait 3 seconds before executing to make sure page is loaded.
         removeItems();
     },3000);
-
-
-    //removeItems();
-    //setInterval(,4000);
 };
