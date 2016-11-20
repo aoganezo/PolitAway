d8177ea7bb9dad7c30314deca844e13539d57e4d
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2016-11-20 04:35:34 -0600

    Engaged local storage!

--- options.js
+++ options.js
@@ -1,45 +1,54 @@
+levelglobal = "";
+
+function saveLevel(agressivness){
+        level = ""
+        if (agressivness <= 33){
+            level = "low"
+        }
+
+        else if (agressivness < 66){
+            level = "med"
+        }
+
+        else if (agressivness >= 66){
+            level="high"
+        }
+         chrome.storage.sync.set({"level": level},()=>{})
+    }
+
 window.onload = function(){
-console.log(localStorage.getItem("agressivness"));
-window.level;
+    console.log(chrome.storage.sync.get("agressivness",()=>{}));
+    var level = "";
     
-    var agressivness = localStorage.getItem("agressivness")
+    var agressivness = chrome.storage.sync.get("agressivness",()=>{});
     console.log(agressivness);
     $("a[role='slider']").css({left: agressivness + "%"})
     document.getElementById("points").value = (agressivness);
     agressivness = parseInt(agressivness);   
 
-if (agressivness === null){
-    console.log("NULL")
-    var agressivness = 100;
-    localStorage.setItem("agressivness", agressivness);
-    var agressivness = parseInt(agressivness);
-}    
-    //saveOptions 
+    if (agressivness === null){
+        console.log("NULL")
+        var agressivness = 100;
+        chrome.storage.sync.set({"agressivness": agressivness},()=>{});
+        var agressivness = parseInt(agressivness);
+    }    
+        //saveOptions 
     document.getElementById("save").addEventListener('click', function(){
-    agressivness = document.getElementById("points").value;
-    localStorage.setItem("agressivness", agressivness);
-    console.log(localStorage.getItem("agressivness"));
-});
+        agressivness = document.getElementById("points").value;
+        chrome.storage.sync.set({"agressivness": agressivness},()=>{});
+        console.log(chrome.storage.sync.get("agressivness",()=>{}));
+        saveLevel(agressivness);
+        });
     //resetOptions
     document.getElementById("reset").addEventListener('click',
 function(){
-    agressivness = 100;
-    document.getElementById("points").value = (agressivness);
-    localStorage.setItem("agressivness", agressivness);
-    $("a[role='slider']").css({left: "100" + "%"})
-});
+        agressivness = 100;
+        document.getElementById("points").value = (agressivness);
+        chrome.storage.sync.set({"agressivness": agressivness},()=>{});
+        $("a[role='slider']").css({left: "100" + "%"})
+    });
 
-function level(){
-    if (agressivness <= 33){
-        level = "low"
-    }
     
-    else if (agressivness < 66){
-        level = "med"
-    }
-    
-    else if (agressivness >= 66){
-        level="high"
-    }
-}
 };
+
+
