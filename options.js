89ef5ecd8602fd84693202217b163fa73ebf5958
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2016-11-20 03:36:07 -0600

    Options page fully compatible with Chrome Extension permissions. Global variable level added.

--- options.js
+++ options.js
@@ -1,41 +1,33 @@
 window.onload = function(){
 console.log(localStorage.getItem("agressivness"));
-var level;
-
-function loadOptions() {
+window.level;
+    
     var agressivness = localStorage.getItem("agressivness")
     console.log(agressivness);
     $("a[role='slider']").css({left: agressivness + "%"})
-    
-    
     document.getElementById("points").value = (agressivness);
- agressivness = parseInt(agressivness);   
-}
+    agressivness = parseInt(agressivness);   
+
 if (agressivness === null){
     console.log("NULL")
     var agressivness = 100;
     localStorage.setItem("agressivness", agressivness);
     var agressivness = parseInt(agressivness);
-}
-
-//function saveOptions(){
-//    agressivness = document.getElementById("points").value;
-//    localStorage.setItem("agressivness", agressivness);
-//    console.log(localStorage.getItem("agressivness"));
-//}
-    
-    document.getElementById("points").addEventListener('click', function(){
+}    
+    //saveOptions 
+    document.getElementById("save").addEventListener('click', function(){
     agressivness = document.getElementById("points").value;
     localStorage.setItem("agressivness", agressivness);
     console.log(localStorage.getItem("agressivness"));
 });
-
-function eraseOptions() {
+    //resetOptions
+    document.getElementById("reset").addEventListener('click',
+function(){
     agressivness = 100;
-    document.getElementById("points").value = agressivness;
+    document.getElementById("points").value = (agressivness);
     localStorage.setItem("agressivness", agressivness);
     $("a[role='slider']").css({left: "100" + "%"})
-}
+});
 
 function level(){
     if (agressivness <= 33){
